2ffe27cfa0183f31d0c10f04a1a99f436b1bf698
Bertrand Shao <bertrandshao@Bertrands-MacBook-Pro.local>
2020-07-02 17:30:26 -0700

    Create Redux store

---
+++ squad-goals-client/dbschema.js
@@ -0,0 +1,12 @@
+let db = {
+    users: [
+      {
+        userId: 'Ch9lxDoVNldwc4Ew3ETQqz6oxMn2',
+        email: 'berty@gmail.com',
+        handle: 'Bertrand',
+        createdAt: '2020-07-02T23:57:39.958Z',
+        imageUrl: 'image/dsfsdkfghskdfgs/dgfdhfgdh',
+        challenges: []
+      }
+    ]
+}
