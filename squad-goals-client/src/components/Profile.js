e86d321a88519a9774832b9945c3b0d05c0331fd
Bertrand Shao <shaob@g.ucla.edu>
2020-07-14 17:30:06 -0700

    Make edits to profile card

--- squad-goals-client/src/components/Profile.js
+++ squad-goals-client/src/components/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Loading from './Loading';
 //MUI
 import Button from '@material-ui/core/Button';
 import withStyles from '@material-ui/core/styles/withStyles';
@@ -20,17 +21,59 @@ const styles = {
     paper: {
         backgroundColor: '#ffffff',
         color: 'black',
+        height: '420px',
+        width: '280px',
+        position: 'relative',
+        textAlign: 'center',
+        padding: '10px',
         // textShadow: '1px 1px black'
     },
     profileImage: {
-        width: 200,
-        height: 200,
+        width: '230px',
+        height: '230px',
+        marginTop: '10%',
         objectFit: 'cover',
+        borderRadius: '50%',
         maxWidth: '100%',
-        borderRadius: '50%'
+        maxHeight: '100%'
     },
     imageWrapper: {
-        margin: 20
+        marginTop: '-10px',
+        marginRight: '20px',
+        marginLeft: '20px',
+        marginBottom: '20px',
+    },
+    camera: {
+        color: 'grey',
+        position: 'absolute',
+        left: '210px',
+        bottom: '185px',
+        // float: 'right',
+    },
+    cameraFrame: {
+        height: '50px',
+        width: '50px',
+        backgroundColor: 'white',
+        borderRadius: '50%',
+        textAlign: 'center',
+        // float: 'right',
+    },
+    rounded: {
+        borderTop: '3px solid black',
+        marginLeft: '40px',
+        marginRight: '40px',
+        borderRadius: '10px',
+        marginTop: '10px',
+        marginBottom: '20px',
+    },
+    noAccount: {
+        height: '90px',
+        width: '250px',
+        paddingTop: '20px',
+        textAlign: 'center'
+    },
+    noButton: {
+        margin: '10px'
     }
 };
 
@@ -47,24 +90,33 @@ export class Profile extends Component {
         fileInput.click();
     }
     render() {
-        const { classes, user: { credentials: { completed, handle, imageUrl }, loading, authenticated } } = this.props;
+        const { classes, user: { credentials: { completed, handle, imageUrl, userId }, loading, authenticated } } = this.props;
 
         let profileMarkup = !loading ? (authenticated ? (
             <Paper className={classes.paper}>
-                <div className={classes.imageWrapper}><img src={imageUrl} alt="profile pic" className={classes.profileImage} /></div>
-                <input type="file" id="imageInput" hidden="hidden" onChange={this.handleImageChange} />
-                <Tooltip title="Edit profile picture" placement="top">
-                    <IconButton onClick={this.handleEditPicture}>
-                        <AddAPhotoIcon color="secondary" />
-                    </IconButton>
-                </Tooltip>
+                <div className={classes.imageWrapper}><img src={imageUrl} alt="profile pic" className={classes.profileImage} />
+                    <input type="file" id="imageInput" hidden="hidden" onChange={this.handleImageChange} />
+                    <Tooltip title="Edit profile picture" placement="top" className={classes.camera}>
+                        <div className={classes.cameraFrame}>
+                            <IconButton onClick={this.handleEditPicture}>
+                                <AddAPhotoIcon />
+                            </IconButton>
+                        </div>
+                    </Tooltip>
+                </div>
                 <Typography variant="h3" align="center">{handle}</Typography>
-                <Typography variant="h4" align="center">Streaks: {completed}</Typography>
+                {/* <Typography align="center">{userId}</Typography> */}
+
+                <hr className={classes.rounded} />
+
+                <Typography variant="h4" align="center"> &#128293; Streaks: {completed}</Typography>
             </Paper>
-        ) : (<Paper><Typography variant="body2" align="center">No Profile found, please login again</Typography>
-            <Button variant="contained" color="primary" component={Link} to="/login">Login</Button>
-            <Button variant="contained" color="secondary" component={Link} to="/signup">Sign Up</Button>
-        </Paper>)) : (<p>loading...</p>)
+        ) : (<Paper className={classes.noAccount}><Typography variant="body2" align="center">No Profile found, please login again</Typography>
+            <div className={classes.buttons}>
+                <Button variant="contained" color="primary" className={classes.noButton} component={Link} to="/login">Login</Button>
+                <Button variant="contained" color="secondary" className={classes.noButton} component={Link} to="/signup">Sign Up</Button>
+            </div>
+        </Paper>)) : (<Loading />)
         return profileMarkup;
     }
 }
