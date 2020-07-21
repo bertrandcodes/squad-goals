899b7cdf36d6e13a295b84d228e7dcaa2bfee5e6
Bertrand Shao <bertrandshao@gmail.com>
2020-07-21 15:28:59 -0700

    Fix CORS problem

--- firebase-server/functions/index.js
+++ firebase-server/functions/index.js
@@ -2,11 +2,11 @@ const functions = require('firebase-functions');
 const FBAuth = require('./util/FBAuth')
 const app = require('express')();
 
-const cors = require('cors');
+const cors = require('cors')({ origin: true });
 app.use(cors());
 
-const { getAllChallenges, postOneChallenge, getChallenge, updateChallenge } = require('./handlers/challenges');
-const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser, addFriend, getFriend } = require('./handlers/users');
+const { getAllChallenges, postOneChallenge, getChallenge, updateChallenge, updateStar, updateTime } = require('./handlers/challenges');
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser, addFriend, getFriend, updateCompleted } = require('./handlers/users');
 
 //Users routes
 app.post('/signup', signup);
@@ -16,12 +16,14 @@ app.post('/user', FBAuth, addUserDetails);
 app.get('/user', FBAuth, getAuthenticatedUser);
 app.put('/user', FBAuth, addFriend);
 app.get('/user/:friendUid', FBAuth, getFriend);
-
+app.put('/user/:handle', FBAuth, updateCompleted);
 
 //Challenges routes
 app.get('/challenges', getAllChallenges);
 app.post('/challenge', FBAuth, postOneChallenge);
 app.get('/challenge/:challengeId', getChallenge);
 app.put('/challenge/:challengeId', updateChallenge);
+app.put('/challenge/:challengeId/star', updateStar);
+app.put('/challenge/:challengeId/time', updateTime);
 
 exports.api = functions.https.onRequest(app);
